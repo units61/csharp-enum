1cbd98b5932bdf3f5931577433a78f3f7e06d0ec
Your Name <you@example.com>
2022-10-15 14:58:38 +0300

    ADD README

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace csharp_enum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Gunler.Pazar);
+            Console.WriteLine((int)Gunler.Cumartesi);
+
+            int sıcaklık =32;
+
+            if(sıcaklık <= (int)HavaDurumu.Normal)
+                Console.WriteLine("Dışarıya çıkmak için biraz daha ısınmasını bekliyelim.");
+            else if(sıcaklık >= (int)HavaDurumu.Sıcak)
+                Console.WriteLine("Dışarıya çıkmak için çok sıcak bir gün.");
+            else if(sıcaklık>=(int)HavaDurumu.Normal && sıcaklık<(int)HavaDurumu.CokSicak)
+                Console.WriteLine("Hadi dışarıya çıkalım.");
+
+        }
+    }
+
+    enum Gunler
+    {
+        Pazartesi=1,
+        Salı,
+        Çarşamba,
+        Perşembe,
+        Cuma=23,
+        Cumartesi,
+        Pazar
+    }
+
+    enum HavaDurumu
+    {
+        Soguk = 5,
+        Normal=20,
+        Sıcak=25,
+        CokSicak =30
+    }
+}
